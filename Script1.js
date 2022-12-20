c6d86b16638553c942aa74b1dd6462a6eb476e6a
deepak <deepak.kondapalli29@gmail.com>
2022-12-20 23:34:52 +0530

    first commit

---
+++ Script1.js
@@ -0,0 +1,3 @@
+// creating name, designation, age, salary, city for a person in a single line
+var name = "Deepak", designation = "Full stack developer", age = 21, salary = 40000, city = "hyderabad";
+console.log(name, designation, age, salary, city); 
---
+++ script2.js
@@ -0,0 +1,10 @@
+var number = 0;
+if (number==0){
+    console.log("Neither Odd nor Even");
+}
+else if (number%2==0){
+    console.log("Even");
+}
+else{
+    console.log("Odd");
+}
